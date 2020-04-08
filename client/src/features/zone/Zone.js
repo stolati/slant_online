6080f46618a047ce9a322b2a2a3332f3f9006cb3
Mickael Kerbrat <mickael.kerbrat@gmail.com>
2020-04-07 18:38:56 -0700

    added loop detections

--- client/src/features/zone/Zone.js
+++ client/src/features/zone/Zone.js
@@ -9,16 +9,184 @@ import {
     pushAnswer,
 } from './zoneSlice';
 import styles from './Zone.module.css';
-import { range, getRelativeCoordinates, postphoneAsync } from '../../utils';
+import { range, getRelativeCoordinates } from '../../utils';
 
 // Calculation simplification
 
-const CONFIG = { //C is for constants
-    border: 10, // Every sides
-    box_width: 10,
-    box_height: 10,
-    circle: 4,
-};
+//const CONFIG = { //C is for constants
+//    border: 10, // Every sides
+//    box_width: 10,
+//    box_height: 10,
+//    circle: 4,
+//};
+
+
+const NUMBER_STATE = {
+    INVALID: 'red',
+    SOLVED: 'green',
+    DEFAULT: 'black',
+}
+
+let getNumberColor = (problem, solution, posX, posY) => {
+
+    let num = parseInt(problem[posY][posX]);
+
+    let getSol = (y, x, inverted)=>{
+        let solY = solution[y];
+        if(solY === undefined){
+            return undefined;
+        }
+        let solYX = solY[x];
+        if(solYX === undefined){
+            return undefined;
+        }
+
+        if(solYX === ' ')
+            return null;
+        if(solYX === '/')
+            return !inverted;
+        if(solYX === '\\')
+            return inverted;
+
+        throw new Error();
+    }
+
+    let around = [
+        getSol(posY-1, posX-1, true),
+        getSol(posY-1, posX, false),
+        getSol(posY, posX-1, false),
+        getSol(posY, posX, true),
+    ].filter(e=>e!==undefined);
+
+    let numConnection = around.filter(e=>e===true).length;
+    let numNonConnection = around.filter(e=>e===false).length;
+    let numTotal = around.length;
+
+    let allowedNumNonConnection = numTotal - num;
+
+    if(numConnection > num || numNonConnection > allowedNumNonConnection)
+        return NUMBER_STATE.INVALID;
+
+    if(numConnection === num && numNonConnection === allowedNumNonConnection){
+        return NUMBER_STATE.SOLVED;
+    }
+
+    return NUMBER_STATE.DEFAULT;
+}
+
+
+let getLoopSolution = (solution) => {
+
+    //Generate a result from the solution, with content being false or null;
+   let res = solution.map((line, y) => {
+      return line.map((cell, x) => {
+        return solution[y][x] !== ' ';
+      })
+   });
+
+   let getPos = (x, y) => {
+        let solY = solution[y];
+        if(solY === undefined)
+            return ' ';
+        let solYX = solution[y][x];
+        if(solYX === undefined)
+            return ' ';
+
+        if(res[y][x] === false){
+            return ' ';
+        }
+        return solYX;
+    }
+
+    let getLinked = (x, y) => {
+        //Pos:
+        // 1 2 3
+        // 4 5 6
+        // 7 8 9
+
+        let v1p = {x: x-1, y: y-1}; let v2p = {x: x  , y: y-1}; let v3p = {x: x+1, y: y-1};
+        let v4p = {x: x-1, y: y  };                           let v6p = {x: x+1, y: y  };
+        let v7p = {x: x-1, y: y+1}; let v8p = {x: x  , y: y+1}; let v9p = {x: x+1, y: y+1};
+
+        let v1 = getPos(x-1, y-1); let v2 = getPos(x  , y-1); let v3 = getPos(x+1, y-1);
+        let v4 = getPos(x-1, y  ); let v5 = getPos(x  , y  ); let v6 = getPos(x+1, y  );
+        let v7 = getPos(x-1, y+1); let v8 = getPos(x  , y+1); let v9 = getPos(x+1, y+1);
+
+        if(v5 === '/') {
+            return [
+                [
+                    v2 === '\\' ? v2p : null, // 2
+                    v3 === '/' ? v3p : null, // 3
+                    v6 === '\\' ? v6p : null, //6
+                ].filter(e=>e !==null),
+                [
+                    v4 === '\\' ? v4p : null, // 4
+                    v7 === '/' ? v7p : null, // 7
+                    v8 === '\\' ? v8p : null, // 8
+                ].filter(e=>e !==null)
+            ].filter(e=>e.length !== 0);
+        }
+
+        if(v5 === '\\'){
+            return [
+                [
+                    v1 === '\\' ? v1p : null, // 1
+                    v2 === '/' ? v2p : null, // 2
+                    v4 === '/' ? v4p : null, //4
+                ].filter(e=>e !==null),
+                [
+                    v6 === '/' ? v6p : null, // 6
+                    v8 === '/' ? v8p : null, // 8
+                    v9 === '\\' ? v9p : null, // 9
+                ].filter(e=>e !==null)
+            ].filter(e=>e.length !== 0);
+        }
+
+
+
+
+//        return [
+//            (v1 === '\\' && v5 === '\\') ? v1p : null,
+//            ((v2 === '/' && v5 === '\\') || (v2 === '\\' && v5 === '/') ) ? v2p : null,
+//            (v3 === '/' && v5 === '/') ? v3p : null,
+//            ((v4 === '/' && v5 === '\\') || (v4 === '\\' && v5 === '/')) ? v4p : null,
+//            false ? v5p : null,
+//            ((v6 === '/' && v5 === '\\') || (v6 === '\\' && v5 === '/')) ? v6p : null,
+//            (v7 === '/' && v5 === '/') ? v7p : null,
+//            ((v8 === '/' && v5 === '\\') || (v8 === '\\' && v5 === '/')) ? v8p : null,
+//            (v9 === '\\' && v5 === '\\') ? v9p : null,
+//        ].filter(e => e !== null)
+    }
+
+    let processElem = (x, y)=>{
+        let e = getPos(x, y);
+        if(e === ' ') return;
+        let linked = getLinked(x, y);
+
+        if(linked.length === 2){
+            return;
+        }
+
+        res[y][x] = false;
+
+        if(linked.length === 0){
+            return;
+        }
+
+        let nexts = linked[0];
+        if(nexts.length === 1){
+            processElem(nexts[0].x, nexts[0].y);
+        }
+    }
+
+   solution.forEach((line, y) => {
+      line.forEach((_, x) => processElem(x, y))
+   });
+
+
+   return res;
+
+}
 
 
 
@@ -42,6 +210,8 @@ export function Zone(props) {
 
    let isFull = !solution.some(line => line.some((cell) => cell === ' '));
 
+   let loopSolution = getLoopSolution(solution);
+
    // TODO : Here we should check for errors
    // There are : - loops and numbers
    // Then when getting the result, we should have an animation saying it's good.
@@ -59,7 +229,6 @@ export function Zone(props) {
    let viewBoxHeight = ((height)*M) + B + B;
 
    const viewBox = `0 0 ${viewBoxWidth} ${viewBoxHeight}`;
-   console.log(viewBox);
 
 
    let onMouseClickEvent = (e, left) => {
@@ -83,7 +252,7 @@ export function Zone(props) {
 
       //Removing events outside the field
       if(posInField.x < 0 || posInField.y < 0 || posInField.x > (width *M) || posInField.y > (height * M)){
-        return;
+        return true;
       }
 
       let posAbs = {
@@ -145,11 +314,13 @@ export function Zone(props) {
                     return range(height).map((_, y) => {
                          let pos = `${x},${y}`;
 
+                         let curStyle = loopSolution[y][x] ? styles.lineError : styles.line;
+
                          let antislash =
-                         <line x1={(x*M)+B} y1={(y*M)+B} x2={(x*M)+B+M} y2={(y*M)+B+M} className={styles.line} key={"/"+ pos}/>;
+                         <line x1={(x*M)+B} y1={(y*M)+B} x2={(x*M)+B+M} y2={(y*M)+B+M} className={curStyle} key={"/"+ pos}/>;
 
                          let slash =
-                         <line x1={(x*M)+B+M} y1={(y*M)+B} x2={(x*M)+B} y2={(y*M)+B+M} className={styles.line} key={'\\' +pos}/>;
+                         <line x1={(x*M)+B+M} y1={(y*M)+B} x2={(x*M)+B} y2={(y*M)+B+M} className={curStyle} key={'\\' +pos}/>;
 
                          let sol_content = solution[y][x];
 
@@ -177,7 +348,9 @@ export function Zone(props) {
                                 return null;
                           }
 
-                          return <circle cx={(x*M)+B} cy={(y*M)+B} r={CS/1.5} stroke="black" strokeWidth="1" fill="grey" key={pos}/>
+                          let numberColor = getNumberColor(problem, solution, x, y);
+
+                          return <circle cx={(x*M)+B} cy={(y*M)+B} r={CS/1.5} stroke={numberColor} strokeWidth="1" fill="grey" key={pos}/>
 
                     }).flat()
                  })
@@ -194,7 +367,9 @@ export function Zone(props) {
                                 return null;
                           }
 
-                          return <text x={(x*M)+B-(CS/2)+1-0.25} y={(y*M)+B+(CS/2)-1+.5} className={styles.small} key={pos} draggable="false">
+                          let numberColor = getNumberColor(problem, solution, x, y);
+
+                          return <text x={(x*M)+B-(CS/2)+1-0.25} y={(y*M)+B+(CS/2)-1+.5} className={styles.small_text} key={pos} draggable="false" fill={numberColor}>
                               {problem[y][x]}
                           </text>;
 
